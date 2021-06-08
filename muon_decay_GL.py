a2e1568c0b7e4630664bb919529e4c99717b2b7a
GLaurin <lauringuillaume@gmail.com>
2021-06-08 09:33:35 -0400

    Mise en fonction de l'integration et de l'identification des pic

--- muon_decay_GL.py
+++ muon_decay_GL.py
@@ -10,24 +10,25 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.signal import find_peaks
 
-#%% Analyse
+#%%
 
-seuil = 0.005
-for i in [0]:
-    data = np.loadtxt("C:\\Users\\lauri\\Documents\\Sessions et Stages\\2021-2É\\Muon Decay\\acq_test\\te465.txt")
-    x = data[:,0]
-    y = data[:,1]
+def IntegrateData_FindPeaks(x,y, l_min=4, l_max=12, c_box=6, seuil=0.005, figshow=True, saveID=None):
+    """
+    Integration des donnees par intervale variant entre l_min et l_max
+    Integration des donnees par intervale fixe de grandeur c_box
+    Identification des pics inferieurs au seuil
+    Enregistrement de la figure si saveID est donne
+    Affichage de la figure si figshow est True
+    """
 
-    l_min       = 4                         # Largeur minimale d'integration
-    l_max       = 16                        # Largeur maximale d'integration
     l_box       = l_max                     # Largeur d'integration initiale
-    c_box       = 7                         # Largeur d'integration constante
     y_integre_1 = np.zeros(y.size)          # y integre
     y_integre_2 = np.zeros(y.size)          # y integre variable
     box_std_p   = 0                         # Initialisation de l'ecart-type d'intervalle
-    bkg_std     = np.zeros(l_max-l_min+1)   # Ecart-type seuil du bruit de fond
     box_arr     = np.zeros(y.size)          # Dimensions des intervales d'integration
 
+    # Ecart-type seuil du bruit de fond
+    bkg_std     = np.zeros(l_max-l_min+1)
     for j in range(bkg_std.size):
         l_box      = l_min+j
         bkg_std[j] = np.concatenate((y[0:200][y[0:200].argsort()[:200-(l_box-l_box//2):-1]], y[0:200][y[0:200].argsort()[:l_box//2]])).std()
@@ -54,28 +55,49 @@ for i in [0]:
         box_arr[j]     = l_box
 
     peaks,_ = find_peaks(-y_integre_2, height=seuil)
+    if peaks.size>2: 
+        print("Plus de 2 minima locaux identifiees. \nVerifiez le seuil.")
 
     ## Figure
-    fig,ax = plt.subplots(figsize=(10,8))
-    ax.plot(y,'k.', label="0")
-    ax.plot(y_integre_1, c='b', label=c_box)
-    ax.plot(y_integre_2, c='r', label="variable")
-    ax.legend(title="intervale d'integration")
-    ax.set_xlim(200,600)
-    for i in range(peaks.size):
-        ax.axvline(peaks[i],c='k',ls=':',alpha=0.8)
-    
-    ## Zoom manuel sur le pic
-    axin = ax.inset_axes([0.31,0.27,0.6,0.5])
-    axin.plot(y,'k.')
-    axin.plot(y_integre_1, c='b')
-    axin.plot(y_integre_2, c='r')
-    axin.set_xlim(500,580)
-    axin.set_ylim(-0.04,0.01)
-    ax.indicate_inset_zoom(axin, edgecolor="black")
-    for i in range(peaks.size):
-        axin.axvline(peaks[i],c='k',ls=':',alpha=0.8)
+    if saveID or figshow:
+        fig,ax = plt.subplots(figsize=(10,8))
+        ax.plot(y,'k.', label="0")
+        ax.plot(y_integre_1, c='b', label=c_box)
+        ax.plot(y_integre_2, c='r', label="variable")
+        ax.legend(title="intervale d'integration",loc=3,framealpha=1)
+        ax.set_xlim(200,600)
+        for i in range(peaks.size):
+            ax.axvline(peaks[i],c='k',ls=':',alpha=0.8)
+        
+        if peaks.size==2:
+            ## Zoom manuel sur le pic
+            axin = ax.inset_axes([0.35,0.08,0.6,0.5])
+            axin.plot(y,'k.')
+            axin.plot(y_integre_1, c='b')
+            axin.plot(y_integre_2, c='r')
+            axin.set_xlim(peaks[-1]-50,peaks[-1]+50)
+            axin.set_ylim(-0.04,0.01)
+            ax.indicate_inset_zoom(axin, edgecolor="black")
+            for i in range(peaks.size):
+                axin.axvline(peaks[i],c='k',ls=':',alpha=0.8)
+            axin.axhline(-seuil,c='r',ls=':',alpha=0.8)
+            axin.text(570,-seuil*1.1,"seuil",va="top",color='r')
 
-    plt.show()
+        if saveID:
+            plt.savefig(saveID)
+            plt.close()
+        
+        if figshow:
+            plt.show()
 
-# %%
+    return y_integre_1, y_integre_2, peaks, box_arr
+
+
+#%% Analyse
+
+data = np.loadtxt("C:\\Users\\lauri\\Documents\\Sessions et Stages\\2021-2É\\Muon Decay\\acq_test\\te465.txt")
+x = data[:,0]
+y = data[:,1]
+
+y_var, y_con, ip, box = IntegrateData_FindPeaks(x,y)
+
