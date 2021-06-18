64f98260f828bc37a22f35fee64b3545b8e7b3d3
saschazb <saschazb@gmail.com>
2021-06-18 16:25:25 -0400

    Update avec fichier t_decay_sample

--- muon_decay_time_FFA.py
+++ muon_decay_time_FFA.py
@@ -8,11 +8,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.signal import find_peaks
 import os
+from scipy.optimize import curve_fit
 #%%
 def MuonCount(t,N0,lambd):
     return N0*np.exp(-lambd*t)
 
 def main(x, y, seuil, figshow = False, figsave = False, saveID = None):
+    
     peaks,_ = find_peaks(-y, height = seuil)
     peaks   = np.delete(peaks,np.where(peaks<240)[0])
     ip = 0  
@@ -21,6 +23,7 @@ def main(x, y, seuil, figshow = False, figsave = False, saveID = None):
         if dp<30:
             peaks = np.delete(peaks,ip+1)
         else: ip += 1
+        
     if figsave or figshow:
         fig,ax = plt.subplots(figsize=(10,8))
         ax.plot(y,'k.', label="Donnees brutes")
@@ -31,36 +34,50 @@ def main(x, y, seuil, figshow = False, figsave = False, saveID = None):
         ax.legend(framealpha=1, loc=3)
 
         if peaks.size>1 and figsave:
-            plt.savefig(saveID)
+            if saveID == None:
+                print("ATTENTION!!! Donner un saveID")
+            else:
+                plt.savefig(saveID)
         if figshow:
             plt.show()
         else:
             plt.close()
     
-    return
+    return peaks
 #%%
 file_prefixe = "acq"
 file_ext     = ".txt"
 folder       = "C:\\Users\\sasch\\Desktop\\Stage été 2021\\codes\\muon_decay_FFA-50-50\\"
 N_data       = len(os.listdir(folder))-1
+t_decay = np.zeros(0)   # Temps de desintegration des evenements identifies
 
 for i in range(N_data):
-    file_id = str(i+18900)
+    file_id = str(i+1)
     if len(file_id)<6: 
         file_id = (6-len(file_id))*"0"+file_id
         file_path = folder + file_prefixe + file_id + file_ext
     data = np.loadtxt(file_path)
     
-    x       = data[:,0]   #La première colonne des acquisitions représente le temps
-    y       = data[:,1]   #La deuxième colonne représente l'amplitude du signal
+    try:
+        x       = data[:,0]   #La première colonne des acquisitions représente le temps
+        y       = data[:,1]   #La deuxième colonne représente l'amplitude du signal
+    except:
+        print(file_id)
+        continue
+    
     seuil   = 0.02
-    fshow   = False
-    fsave   = True
+    fshow   = 0
+    fsave   = 1
     sid     = folder+"Decays\\figure"+file_id+"_seuil"+str(seuil)[2:]
     
     ip = main(x, y, seuil, figshow = fshow, figsave = fsave, saveID = sid) #Appel à a fonction
     
-    t_decay = np.zeros(0)   # Temps de desintegration des evenements identifies
     if ip.size==2:
         t_decay = np.append(t_decay,x[ip[1]]-x[ip[0]])
-    
+N, bins = np.histogram(t_decay, bins='auto')
+t       = bins[:-1]+ 0.5*(bins[1:] - bins[:-1])
+plt.plot(t, N, 'or', alpha=1)
+plt.savefig(folder+"Decays\\Histogramme_désintégrations")
+plt.close()
+
+popt, pcov = curve_fit(MuonCount,N,t)
